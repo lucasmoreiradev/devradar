410ddb96b48c7e76ba96a29da1166d9ee4a846e1
Lucas Moreira <eskerlucas1@gmail.com>
2020-01-16 17:18:29 -0300

    Add axios & frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,14 +3,17 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express()
+
 
 mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
+app.use(cors())
 app.use(express.json())
 
 app.use(routes)
